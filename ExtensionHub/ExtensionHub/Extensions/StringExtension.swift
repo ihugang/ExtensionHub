d0f8ab1e0b3889c434116339739c51a199f26a9c
胡刚 <ihugang@gmail.com>
2015-12-08 22:31:41 +0800

    String Extension

---
+++ ExtensionHub/ExtensionHub/Extensions/StringExtension.swift
@@ -0,0 +1,21 @@
+//
+//  StringExtension.swift
+//  ExtensionHub
+//
+//  Created by HuGang on 15/12/8.
+//  Copyright © 2015年 Codans Software. All rights reserved.
+//
+
+import UIKit
+
+class StringExtension: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
